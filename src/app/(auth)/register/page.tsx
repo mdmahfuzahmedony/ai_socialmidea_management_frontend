e4e53c40e3614f60046866c97c35213630f96cb0
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2026-07-30 10:43:52 +0600

    [update] login page and register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+// src/app/(auth)/login/page.tsx
+
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>register Page</h1>
+      {/* আপনার লগইন ফর্ম এখানে থাকবে */}
+    </div>
+  );
+}
